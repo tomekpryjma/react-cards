358df8b7548cb70eeb335c7c3fc8d26593822871
Tomek <tompe97@tlen.pl>
2020-10-17 14:08:38 +0100

    Amend main app structure & add basic listing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.scss';
+import Card from './components/Card';
+require('dotenv').config();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [cards, setCards] = useState([]);
+    const [cardBackground, setCardBackground] = useState({});
+    const urlBase = process.env.REACT_APP_DOMAIN;
+    const customRestNamespace = '/wp-json/barker/v1';
+    const wpRestNamespace = '/wp-json/wp/v2';
+    const cardBackgroundRoute = urlBase + customRestNamespace + '/card-background';
+    const cardsRoute = urlBase + wpRestNamespace + '/cards-votive';
+    const cardsParams = {params: {per_page: 100}};
+
+    useEffect(() => {
+        axios.get(cardBackgroundRoute).then(response => {
+            setCardBackground(response.data);
+        });
+
+        axios.get(cardsRoute, cardsParams).then(response => {
+            console.log(response)
+            setCards(response.data);
+        });
+    }, []);
+
+    return (
+        <div className="App">
+            {
+                cards.map((card, index) => {
+                    return(
+                        <Card
+                            key={index}
+                            card_image_url={card.card_image_url}
+                            card_background_url={cardBackground}
+                        />
+                    );
+                })
+            }
+        </div>
+    );
 }
 
 export default App;
