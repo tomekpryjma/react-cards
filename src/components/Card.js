9837e621a7dffeda049dc1ea5d7cac536e4e1a30
Tomek <tompe97@tlen.pl>
2020-10-17 15:40:19 +0100

    Add more content to Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import gsap, { TimelineMax, TimelineLite } from 'gsap';
 
 function Card(props) {
+    const [flipped, setFlipped] = useState(false);
+    const [timeline] = useState(gsap.timeline({paused: true}));
+    const frontElement = useRef(null);
+    const backElement = useRef(null);
+
+    useEffect(() => {
+        timeline
+            .set(frontElement.current, {rotationY: -180})
+            .to(backElement.current, {rotationY: 180})
+            .to(frontElement.current, {rotationY: 0})
+            .reverse()
+    }, []);
+
+    const onClick = () => {
+        timeline.reversed(! timeline.reversed());
+    }
+
     return(
-        <div className="card">
-            <div className="front" style={{backgroundImage: `url(${props.card_image_url})`}}></div>
-            <div className="back" style={{backgroundImage: `url(${props.card_background_url})`}}></div>
+        <div className="card" onClick={onClick}>
+            <div
+                className="front"
+                style={{backgroundImage: `url(${props.card_image_url})`}}
+                ref={frontElement}
+            ></div>
+            <div
+                className="back"
+                style={{backgroundImage: `url(${props.card_background_url})`}}
+                ref={backElement}
+            ></div>
         </div>
     );
 }
