a94c42b17db2c557d6fb33f1de90f1b1dff82353
Tomek <tompe97@tlen.pl>
2020-10-18 12:19:20 +0100

    Flesh out the functionality & animations

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,29 +1,111 @@
 import React, { useEffect, useRef, useState } from 'react';
 import gsap from 'gsap';
-import CardText from './CardText';
+import { TweenMax } from 'gsap/gsap-core';
 
 function Card(props) {
     const [flipped, setFlipped] = useState(false);
     const [timeline] = useState(gsap.timeline({paused: true}));
+    const [fadeTimeline] = useState(gsap.timeline({paused: true}));
+    const [expandTimeline] = useState(gsap.timeline({paused: true}));
+    const [readMoreIsExpanded, setReadMoreIsExpanded] = useState(false);
+    const cardElement = useRef(null);
+    const wrapperElement = useRef(null);
+    const textWrapper = useRef(null);
     const frontElement = useRef(null);
     const backElement = useRef(null);
+    const textElement = useRef(null);
 
     useEffect(() => {
-        timeline
-            .set(frontElement.current, {rotationY: -180})
-            .to(backElement.current, {rotationY: 180})
-            .to(frontElement.current, {rotationY: 0})
-            .reverse()
+        timeline.clear().to(cardElement.current, {rotationY: -180}).reverse()
+
+        fadeTimeline.clear().fromTo(
+            wrapperElement.current,
+            {opacity: 0},
+            {opacity: 1}
+        );
+        TweenMax.set(textWrapper.current, {height: 0});
     }, []);
 
-    const onClick = () => {
-        timeline.reversed(! timeline.reversed());
-        setFlipped(! timeline.reversed());
+    useEffect(() => {
+        TweenMax.set(textWrapper.current, {height: 0});
+        expandTimeline
+            .clear()
+            .to(
+                textWrapper.current,
+                {
+                    height: textElement.current.clientHeight,
+                    ease: 'expo.inOut'
+                }
+            ).reverse();
+    }, [props.card_text])
+
+    useEffect(() => {
+        const expandReverseDelay = props.time_delay;
+        const flipReverseDelay = props.time_delay - 0.05;
+
+        if (readMoreIsExpanded == false && flipped == false) {
+            return;
+        }
+
+        if (props.reset) {
+            TweenMax.delayedCall(expandReverseDelay, () => {
+                expandTimeline.reverse()
+            })
+            TweenMax.delayedCall(flipReverseDelay, () => {
+                flipToOriginalPosition();
+                setFlipped(false);
+            })
+        }
+    }, [props.reset])
+
+    useEffect(() => {
+        if (flipped) {
+            fadeTimeline.play();
+        }
+        else {
+            fadeTimeline.reverse();
+            setReadMoreIsExpanded(false)
+        }
+    }, [flipped])
+
+    const flipToOriginalPosition = () => {
+        TweenMax.delayedCall(props.time_delay, () => {
+            timeline.reverse()
+        })
+    }
+
+    const onCardClick = () => {
+        props.update_flipped_cards(props.post_id);
+        if (props.interaction_blocked) {
+            return;
+        }
+
+        if (flipped && readMoreIsExpanded) {
+            const delay = 0.5
+
+            expandTimeline.reverse();
+            props.update_opened_read_mores(props.post_id);
+
+            TweenMax.delayedCall(delay, () => {
+                timeline.reversed(! timeline.reversed());
+                setFlipped(! timeline.reversed());
+            })
+        }
+        else {
+            timeline.reversed(! timeline.reversed());
+            setFlipped(! timeline.reversed());
+        }
+    }
+
+    const onReadMoreTriggerClick = () => {
+        expandTimeline.reversed(! expandTimeline.reversed())
+        setReadMoreIsExpanded(! readMoreIsExpanded);
+        props.update_opened_read_mores(props.post_id);
     }
 
     return(
         <div className="card-wrapper">
-            <div className="card" onClick={onClick}>
+            <div className="card" onClick={onCardClick} ref={cardElement}>
                 <div
                     className="front"
                     style={{backgroundImage: `url(${props.card_image_url})`}}
@@ -35,7 +117,13 @@ function Card(props) {
                     ref={backElement}
                 ></div>
             </div>
-            <CardText card_text={props.card_text} flipped={flipped}/>
+
+            <div className="read-more-wrapper" ref={wrapperElement}>
+                <div className="card-description" ref={textWrapper}>
+                    <p ref={textElement}>{props.card_text}</p>
+                </div>
+                <button className="read-more-trigger" onClick={onReadMoreTriggerClick}>Read more</button>
+            </div>
         </div>
     );
 }
