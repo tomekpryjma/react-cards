ee94d007d5ff514978dd917f6942648f608dd588
Tomek <tompe97@tlen.pl>
2020-10-22 08:53:34 +0100

    Amend accordion transitions

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -2,6 +2,7 @@ import React, { useState, useRef, useEffect } from 'react';
 
 export default function Accordion(props) {
     const [accordionActive, setAccordionActive] = useState(false);
+    const [fadeIn, setFadeIn] = useState(false);
     const [height, setHeight] = useState(0);
     const contentRef = useRef(null);
 
@@ -15,6 +16,20 @@ export default function Accordion(props) {
     }, [accordionActive])
 
     useEffect(() => {
+        if (! height) {
+        }
+    }, [height])
+
+    useEffect(() => {
+        if (props.cardIsFlipped) {
+            setFadeIn(true);
+        }
+        else {
+            setTimeout(() => {
+                setFadeIn(false);
+            }, 500)
+        }
+
         if (! props.cardIsFlipped && accordionActive) {
             toggleAccordion();
         }
@@ -26,8 +41,8 @@ export default function Accordion(props) {
 
     return(
         <div
-            className={`accordion ${accordionActive ? 'active' : ''} ${props.cardIsFlipped ? 'show' : ''}`}
-            style={{transitionDelay: ! props.cardIsFlipped ? '0.5s' : ''}}
+            className={`accordion ${accordionActive ? 'active' : ''} ${fadeIn ? 'show' : ''}`}
+            style={{transitionDelay: ! props.cardIsFlipped ? `${props.delayFlipBack / 2}s` : ''}}
             >
             <button onClick={toggleAccordion}>Read more</button>
             <div className="accordion-inner" style={{height: height}}>
